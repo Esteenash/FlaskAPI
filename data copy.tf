a19c9c0f629f601e9e206dc3d49dab2a871282ef
Esteenash <111358200+Esteenash@users.noreply.github.com>
2023-02-28 16:01:43 +0000

    Add files via upload

---
+++ data copy.tf
@@ -0,0 +1,31 @@
+# AWS AZs Data source
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+# AMI Data Source
+data "aws_ami" "amzlinux2" {
+  most_recent      = true
+  owners           = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-gp2"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+}
+
